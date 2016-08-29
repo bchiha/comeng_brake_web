56608247311768d1a0d51f0b33b1400644262f77
Brian Chiha <brian.chiha@gmail.com>
2016-08-29 22:52:38 +1000

    Added header to index.html

--- www/js/main.js
+++ www/js/main.js
@@ -9,7 +9,7 @@ const STEP_PRESSURE = 35;
 const SIMULATIONS = [{label:"Gauges",data:"gauges"},
 	   {label:"Triple Valve",data:"tripleValve"},
 	   {label:"Brake Valve",data:"brakeValve"}];
-const BC_GAUGE_X = 600, BC_GAUGE_Y = 150, D_GAUGE_X = 850, D_GAUGE_Y = 150;
+const BC_GAUGE_X = 600, BC_GAUGE_Y = 110, D_GAUGE_X = 850, D_GAUGE_Y = 110;
 
 // global variables
 var stage = new createjs.Stage("canvas");
@@ -38,6 +38,7 @@ function init() {
 	currentSimulation = "gauges";
 
 
+
 	//animation heartbeat
 	createjs.Ticker.addEventListener("tick", onTick);
 
@@ -48,11 +49,34 @@ var onTick = function(e) {
 	stage.update(e);
 };
 
+//select simulation type
+var onSimulationType = function(combobox) {
+	if (currentSimulation == "gauges") {
+		focusGauges(false);
+	}
+//	if (activeSimulation != null) {
+//		removeChild(activeSimulation);
+//				activeSimulation = null;
+//			}
+//			switch (combobox.value) {
+//				case "tripleValve" :
+//					addTripleValve();
+//					break;
+//				case "brakeValve" :
+//					addBrakeValve();
+//					break;
+//				case "gauges" :
+//					focusGauges(true);
+//			}
+//			currentSimulation = combobox.value;
+//			stage.focus = null;
+}
+
 //focus or unfocus Gauges
 var focusGauges = function(enlarge = true) {
 	if (enlarge) {
-		createjs.Tween.get(gaugeBrakeCylinder.gauge).to({x:300, y:450, scaleX:1.5, scaleY:1.5}, 1000, createjs.Ease.sineOut);
-		createjs.Tween.get(gaugeDual.gauge).to({x:700, y:450, scaleX:1.5, scaleY:1.5}, 1000, createjs.Ease.sineOut);
+		createjs.Tween.get(gaugeBrakeCylinder.gauge).to({x:300, y:400, scaleX:1.5, scaleY:1.5}, 1000, createjs.Ease.sineOut);
+		createjs.Tween.get(gaugeDual.gauge).to({x:700, y:400, scaleX:1.5, scaleY:1.5}, 1000, createjs.Ease.sineOut);
 	} else {
 		createjs.Tween.get(gaugeBrakeCylinder.gauge).to({x:BC_GAUGE_X, y:BC_GAUGE_Y, scaleX:1, scaleY:1}, 1000, createjs.Ease.sineOut);
 		createjs.Tween.get(gaugeDual.gauge).to({x:D_GAUGE_X, y:D_GAUGE_Y, scaleX:1, scaleY:1}, 1000, createjs.Ease.sineOut);
