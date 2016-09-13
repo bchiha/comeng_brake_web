f53cf16913a7151af839c418e1f87368b3b8a3e0
Brian Chiha <brian.chiha@gmail.com>
2016-09-13 11:08:47 +1000

    Add text to toggle switch

--- www/js/toggle_switch.js
+++ www/js/toggle_switch.js
@@ -11,6 +11,7 @@ this.brakeSimulator = this.brakeSimulator||{};
 		//public properties
 		this.switchBase = new createjs.Container();
 		//private properties
+		this._switch = new createjs.Container();
 		this._onState = false;
 		this._blackBit = new createjs.Shape();
 
@@ -20,12 +21,19 @@ this.brakeSimulator = this.brakeSimulator||{};
 		var redBit = new createjs.Shape();
 		redBit.graphics.beginFill("red").drawRect(5, 5, 30, 30);
 		this._blackBit.graphics.beginFill("black").drawRect(5, 5, 30, 30);
+		var txtState = new createjs.Text("EP C/B ON", "20px Arial", "#00CC00");
+		txtState.lineWidth = 30;
+		txtState.x = 725;
+		txtState.y = 55;
+		txtState.textAlign = "center";
+		txtState.name = "text";
 
-		this.switchBase.scaleY = 0.8;
-		this.switchBase.x = 690;
-		this.switchBase.y = 10;
-		this.switchBase.cursor = "pointer";
-		this.switchBase.addChild(background, redBit, this._blackBit);
+		this._switch.scaleY = 0.8;
+		this._switch.x = 690;
+		this._switch.y = 10;
+		this._switch.cursor = "pointer";
+		this._switch.addChild(background, redBit, this._blackBit, txtState);
+		this.switchBase.addChild(this._switch, txtState);
 
 		this._addEvents();
 	}
@@ -33,10 +41,15 @@ this.brakeSimulator = this.brakeSimulator||{};
 
 	//public methods
 	p.toggleIt = function() {
+		var txtState = this.switchBase.getChildByName("text");
 		this._onState = !this._onState;
 		if (this._onState) {
+			txtState.text = "EP C/B OFF";
+			txtState.color = "#CC0000";
 			createjs.Tween.get(this._blackBit).to({x:30}, 200);
 		} else {
+			txtState.text = "EP C/B ON";
+			txtState.color = "#00CC00";
 			createjs.Tween.get(this._blackBit).to({x:0}, 200);
 		}
 	};
@@ -47,7 +60,7 @@ this.brakeSimulator = this.brakeSimulator||{};
 
 	//private methods
 	p._addEvents = function() {
-		this.switchBase.on("click", function() {
+		this._switch.on("click", function() {
 			this.toggleIt();
 			this.switchBase.dispatchEvent("clickToggle", true);
 		}, this);
